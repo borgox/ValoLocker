ba770b4acd2daea85deef9a1142cb23091b8f101
borgo <112983228+borgox@users.noreply.github.com>
2024-09-07 19:44:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,2 @@
-# Message me to buy the Instalocker
-# Discord: borgo.xyz
-# Website: https://borgoxy.xyz (if offline use other contacts)
+# Closed src sorry >.<
+# Will probably make it open source in the future
